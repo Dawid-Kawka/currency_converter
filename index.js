0713b7a0eb528d49805874a23106695d954e6cc7
Dawid Kawka <d.kawka@op.pl>
2022-02-01 11:06:41 +0100

    Add final part of js code in index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ class ExchangeRates {
 
     url = "https://api.nbp.pl/api/exchangerates/tables/a/last/?format=json";
 
+    tBody = document.querySelector("#ratesTable tbody");
+
     init() {
         this.loadData();
     }
@@ -26,9 +28,27 @@ class ExchangeRates {
         this.no = data.no;
         this.ratesData = data.rates;
 
+        document.querySelector("h3").innerHTML = `Kurs Walut <br>
+        Data: ${this.date} <br>
+        No: ${this.no}`
 
+        for (let v in data.rates) {
+            this.addRateToTable(data.rates[v])
+        }
     }
 
+    addRateToTable(el) {
+        console.log(el)
+
+        let tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td class="align-middle"> ${el.code} </td>
+            <td class="align-middle"> ${el.currency} </td>
+            <td class="align-middle"> ${el.mid.toFixed(2)}zł</td>
+        `;
+
+        this.tBody.appendChild(tr);
+    }
 }
 
 const rates = new ExchangeRates();
