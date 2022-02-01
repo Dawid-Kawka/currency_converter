10f1876cac6586888e0de610a9bb2324beea3b3e
Dawid Kawka <d.kawka@op.pl>
2022-02-01 10:55:00 +0100

    Connect with NBP api and get data in json

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-let url = "https://api.nbp.pl/api/exchangerates/tables/a/last/?format=json"
+window.onload = function () {
+    rates.init();
+}
+
+class ExchangeRates {
+
+    url = "https://api.nbp.pl/api/exchangerates/tables/a/last/?format=json";
+
+    init() {
+        this.loadData();
+    }
+
+    loadData() {
+        fetch(this.url).then((response) => {
+            response.json().then((data) => {
+                this.parseData(data);
+            });
+        });
+    }
+
+    parseData(data) {
+        console.log(data);
+        data = data[0];
+        this.table = data.table;
+        this.date = data.effectiveDate;
+        this.no = data.no;
+        this.ratesData = data.rates;
+
+
+    }
+
+}
+
+const rates = new ExchangeRates();
+
+
+
